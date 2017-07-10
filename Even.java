dc42a6a3d3b02a16b9c490bb0f17829af0d760ca
niveditha <nivibe96@gmail.com>
2017-07-10 15:50:19 +0530

    Create Even.java

---
+++ Even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Even   {
+  
+  public static void main(String args[])
+  {
+    
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+       for(int i=a;i<=b;i++)
+       {
+  if(i%2==0)
+  System.out.println(i);
+  }
+  }
+  }
